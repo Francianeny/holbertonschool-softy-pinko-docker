e4f470a6813e97dd9f006a24eacfc9986a7f1246
Francianeny <nirinasoabozy@yahoo.fr>
2024-09-03 21:43:38 +0200

    update api.py

--- task5/api.py
+++ task5/api.py
@@ -1,17 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Exemple de données à retourner
-    data = {
-        "message": "Hello from Task 5 API!",
-        "task": 5
-        }
-    return jsonify(data)
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify('Hello from Task 5')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
