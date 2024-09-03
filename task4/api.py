29fedbca5a0eba52064500a1268cd87525774298
Francianeny <nirinasoabozy@yahoo.fr>
2024-09-03 14:17:25 +0200

    api.py

--- task4/api.py
+++ task4/api.py
@@ -1,14 +1,11 @@
+# api.py for task4
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/hello')
+@app.route('/api/hello', methods=['GET'])
 def hello():
-    return jsonify({
-        "message": "Hello from Task 4",
-        "status": "Success",
-        "details": "API response from Task 4"
-    })
+    return jsonify({"message": "Hello from task4 backend!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
