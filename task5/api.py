b208b7663c0e3fc657e4a363ac4bb76bf5b779c1
Francianeny <nirinasoabozy@yahoo.fr>
2024-09-03 10:12:06 +0200

    api.py

--- task5/api.py
+++ task5/api.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Bienvenue sur le backend du task 5!"})
+@app.route('/api/hello')
+def hello():
+    return jsonify({
+        "message": "Hello from Task 5",
+        "status": "Success",
+        "details": "API response from Task 5"
+    })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5252)
